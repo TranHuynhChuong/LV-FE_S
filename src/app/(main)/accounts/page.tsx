28c618423a34910101ed0b1789a80759be682f1a
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-05-19 17:54:30 +0700

    Update accounts page interface

--- src/app/(main)/accounts/page.tsx
+++ src/app/(main)/accounts/page.tsx
@@ -4,15 +4,34 @@ import AuthGuard from '@/components/guard/AuthGuard';
 import { useEffect } from 'react';
 import { useBreadcrumb } from '@/context/BreadcrumbContext';
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+import StaffTable from './components/staffTable';
+import CustomerTable from './components/customerTable';
+
 export default function Accounts() {
   const { setBreadcrumbs } = useBreadcrumb();
 
   useEffect(() => {
     setBreadcrumbs([{ label: 'Trang chủ', href: '/' }, { label: 'Tài khoản' }]);
   }, [setBreadcrumbs]);
+
   return (
     <AuthGuard allowedRoles={['Admin']}>
-      <div>Account</div>
+      <div className="w-full h-fit bg-white p-4 shadow-sm rounded-md">
+        <Tabs defaultValue="staff" className="flex flex-1 ">
+          <TabsList className="grid w-fit grid-cols-2">
+            <TabsTrigger value="staff">Nhân viên</TabsTrigger>
+            <TabsTrigger value="cusstomer">Khách hàng</TabsTrigger>
+          </TabsList>
+          <TabsContent value="staff">
+            <StaffTable></StaffTable>
+          </TabsContent>
+          <TabsContent value="cusstomer">
+            <CustomerTable></CustomerTable>
+          </TabsContent>
+        </Tabs>
+      </div>
     </AuthGuard>
   );
 }
