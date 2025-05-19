d2667922ecfd63cf50c1c6cae2d266fabbb98edb
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-05-19 17:58:00 +0700

    Complete create staff usecase

---
+++ src/app/(main)/accounts/staff/page.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import AuthGuard from '@/components/guard/AuthGuard';
+import { useEffect } from 'react';
+import { useBreadcrumb } from '@/context/BreadcrumbContext';
+import { useRouter } from 'next/navigation';
+import api from '@/lib/axiosClient';
+import { toast } from 'sonner';
+import { StaffForm } from '../components/staffForm';
+import { useAuth } from '@/hooks/useAuth';
+
+type Staff = {
+  NV_matKhau: string;
+  NV_vaiTro: string;
+  NV_hoTen: string;
+  NV_email: string;
+  NV_soDienThoai: string;
+};
+
+export default function Staff() {
+  const { setBreadcrumbs } = useBreadcrumb();
+  const { userId } = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    setBreadcrumbs([
+      { label: 'Trang chủ', href: '/' },
+      { label: 'Tài khoản', href: '/accounts' },
+      { label: 'Thêm mới nhân viên' },
+    ]);
+  }, [setBreadcrumbs]);
+
+  const handleOnsubmit = async (data: {
+    fullName: string;
+    phone: string;
+    email: string;
+    role: 'Admin' | 'Manager' | 'Sale';
+    password?: string;
+  }) => {
+    try {
+      const payload = {
+        NV_hoTen: data.fullName,
+        NV_soDienThoai: data.phone,
+        NV_email: data.email,
+        NV_vaiTro: data.role,
+        NV_matKhau: data.password,
+        NV_idNV: userId,
+      };
+
+      await api.post('/users/staff', payload);
+      toast.success('Lưu thành công!');
+      router.back();
+    } catch (error) {
+      router.back();
+      toast.error('Đã xảy ra lỗi!');
+      console.error('Lỗi khi tạo nhân viên:', error);
+    }
+  };
+
+  return (
+    <AuthGuard allowedRoles={['Admin']}>
+      <div className="w-full h-fit p-4">
+        <StaffForm onSubmit={handleOnsubmit} />
+      </div>
+    </AuthGuard>
+  );
+}
